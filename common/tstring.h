72489c4077b21bf06f5b2f24d9c27ef17aa7a282
zukisoft <djp952@gmail.com>
2014-07-05 00:55:28 -0400

    Add string width conversion functions to tstring

--- common/tstring.h
+++ common/tstring.h
@@ -25,6 +25,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace std {
 
@@ -33,6 +34,70 @@ namespace std {
 #else
 	typedef string		tstring;
 #endif
+
+	// make_string (ANSI)
+	//
+	// Converts a null terminated C-style string into an std::string
+	inline string make_string(const char_t* psz) { return string(psz); }
+
+	// make_string (Unicode)
+	//
+	// Converts a null terminated C-style string into an std::string
+	inline string make_string(const wchar_t* psz)
+	{
+		if(psz == nullptr) return string();
+
+		// Create an std::vector big enough to hold the converted string data and convert it
+		vector<char_t> convert(WideCharToMultiByte(CP_THREAD_ACP, 0, psz, -1, nullptr, 0, nullptr, nullptr));
+		WideCharToMultiByte(CP_THREAD_ACP, 0, psz, -1, convert.data(), convert.size(), nullptr, nullptr);
+
+		// Construct an std::string around the converted character data
+		return string(convert.data(), convert.size());
+	}
+
+	// make_wstring (ANSI)
+	//
+	// Converts a null terminated C-style string into an std::wstring
+	inline wstring make_wstring(const char_t* psz)
+	{
+		if(psz == nullptr) return wstring();
+
+		// Create an std::vector big enough to hold the converted string data and convert it
+		vector<wchar_t> convert(MultiByteToWideChar(CP_THREAD_ACP, 0, psz, -1, nullptr, 0));
+		MultiByteToWideChar(CP_THREAD_ACP, 0, psz, -1, convert.data(), convert.size());
+
+		// Construct an std::wstring around the converted character data
+		return wstring(convert.data(), convert.size());
+	}
+
+	// make_wstring (Unicode)
+	//
+	// Converts a null terminated C-style string into an std::wstring
+	inline wstring make_wstring(const wchar_t* psz) { return wstring(psz); }
+	
+	// make_tstring (ANSI)
+	//
+	// Converts a null terminate C-style string into an std::tstring
+	inline tstring make_tstring(const char_t* psz)
+	{
+#ifdef _UNICODE
+		return make_wstring(psz);
+#else
+		return make_string(psz);
+#endif
+	}
+
+	// make_tstring (Unicode)
+	//
+	// Converts a null terminate C-style string into an std::tstring
+	inline tstring make_tstring(const wchar_t* psz)
+	{
+#ifdef _UNICODE
+		return make_wstring(psz);
+#else
+		return make_string(psz);
+#endif
+	}
 }
 
 //-----------------------------------------------------------------------------
