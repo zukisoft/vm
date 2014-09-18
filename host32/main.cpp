ae44b3760b3b3ceea551ee60531105b0fda3fdba
zukisoft <djp952@gmail.com>
2014-09-18 01:34:07 -0400

    Host Processes will accept an inherited event handle in the command line

--- host32/main.cpp
+++ host32/main.cpp
@@ -41,23 +41,31 @@ sys32_context_exclusive_t g_rpccontext;
 //	pszCommandLine		- Pointer to the application command line
 //	nCmdShow			- Initial window show command
 
-int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR pszCommandLine, int nCmdShow)
+int APIENTRY _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
 	RPC_BINDING_HANDLE			binding;				// RPC binding from command line
 	RPC_STATUS					rpcresult;				// Result from RPC function call
 	HRESULT						hresult;				// Result from system call API function
 
-	UNREFERENCED_PARAMETER(hInstance);
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(nCmdShow);
+	// EXPECTED ARGUMENTS:
+	//
+	// [0] - Executable path
+	// [1] - RPC binding string
+	// [2] - Inherited event handle (serialized as an int32_t)
+	if(__argc != 3) return static_cast<int>(ERROR_INVALID_PARAMETER);
 
 	// The only argument passed into the host process is the RPC binding string necessary to connect to the server
-	rpcresult = RpcBindingFromStringBinding(reinterpret_cast<rpc_tchar_t*>(pszCommandLine), &binding);
+	rpcresult = RpcBindingFromStringBinding(reinterpret_cast<rpc_tchar_t*>(__targv[1]), &binding);
 	if(rpcresult != RPC_S_OK) return static_cast<int>(rpcresult);
 
 	// Attempt to acquire the host runtime context handle from the server
 	hresult = sys32_acquire_context(binding, &g_rpccontext);
 	if(FAILED(hresult)) return static_cast<int>(hresult);
+
+	// The server will wait for an inherited event object to be signaled to ensure that this
+	// process was able to acquire the context handle; set that event and close the handle
+	HANDLE signal = reinterpret_cast<HANDLE>(_ttol(__targv[2]));
+	if(!SetEvent(signal) || !CloseHandle(signal)) return static_cast<int>(GetLastError());
 
 	//
 	// TODO: just release the context and exit, more interesting things go here later
