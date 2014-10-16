523669601e2b07f3c8e75be7a19c09396888bca7
zukisoft <djp952@gmail.com>
2014-10-16 01:20:41 -0400

    WIP 10.15.2014
    Begin fleshing out the updated HostFileSystem class

--- service/HostFileSystem.cpp
+++ service/HostFileSystem.cpp
@@ -26,12 +26,113 @@
 #pragma warning(push, 4)
 
 //-----------------------------------------------------------------------------
+// MapHostException (local)
+//
+// Converts a Win32 error code into a representative LinuxException instance
+//
+// Arguments:
+//
+//	code		- Win32 error code to be mapped
+
+static LinuxException MapHostException(DWORD code = GetLastError())
+{
+	int linuxcode = LINUX_EIO;		// Use EIO as the default linux error code
+
+	// Try to map the Win32 error code to something that makes sense
+	switch(code) {
+
+		case ERROR_FILE_NOT_FOUND:		linuxcode = LINUX_ENOENT; break;
+		case ERROR_PATH_NOT_FOUND:		linuxcode = LINUX_ENOENT; break;
+		case ERROR_FILE_EXISTS:			linuxcode = LINUX_EEXIST; break;
+		case ERROR_INVALID_PARAMETER:	linuxcode = LINUX_EINVAL; break;
+		case ERROR_ALREADY_EXISTS:		linuxcode = LINUX_EEXIST; break;
+	}
+
+	// Generate a LinuxException with the mapped code and provide the underlying Win32
+	// error as an inner Win32Exception instance
+	return LinuxException(linuxcode, Win32Exception(code));
+}
+
+//-----------------------------------------------------------------------------
 // HOSTFILESYSTEM IMPLEMENTATION
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
+// HostFileSystem::Mount (static)
+//
+// Mounts the file system on a host system directory object
+//
+// Arguments:
+//
+//	source		- Host path; must reference a directory object
+//	flags		- Standard mounting flags and attributes
+//	data		- Additional file-system specific mounting options
+
+FileSystemPtr HostFileSystem::Mount(const uapi::char_t* source, uint32_t flags, void* data)
+{
+	std::shared_ptr<MountPoint>			mountpoint;			// MountPoint instance
+
+	std::tstring hostpath = std::to_tstring(source);		// Convert source from ANSI/UTF-8
+
+	// Determine the type of node that the path represents; must be a directory for mounting
+	DWORD attributes = GetFileAttributes(hostpath.c_str());
+	if(attributes == INVALID_FILE_ATTRIBUTES) throw LinuxException(LINUX_ENOTDIR);
+	if((attributes & FILE_ATTRIBUTE_DIRECTORY) == 0) throw LinuxException(LINUX_ENOTDIR);
+
+	// Attempt to open a query-only handle against the file system directory object
+	HANDLE handle = CreateFile(hostpath.c_str(), 0, FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, 
+		FILE_FLAG_POSIX_SEMANTICS | FILE_FLAG_BACKUP_SEMANTICS, nullptr);
+	if(handle == INVALID_HANDLE_VALUE) throw MapHostException();
+
+	// Attempt to create the MountPoint instance for this file system against the opened handle
+	try { mountpoint = std::make_shared<MountPoint>(handle, flags, data); }
+	catch(...) { CloseHandle(handle); throw; }
+
+	// Construct the HostFileSystem instance , providing an alias attached to the mountpoint node
+	return std::make_shared<HostFileSystem>(mountpoint, Alias::Construct("", nullptr /* TODO */));
+}
+
+//-----------------------------------------------------------------------------
 // HOSTFILESYSTEM::ALIAS IMPLEMENTATION
 //-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// HostFileSystem::Alias::Construct (static)
+//
+// Constructs a new Alias instance
+//
+// Arguments:
+//
+//	name		- Name to assign to this Alias instance
+//	parent		- Parent alias for this Alias instance, or nullptr if this is root
+//	node		- Node instance to be referenced by this Alias instance
+
+std::shared_ptr<HostFileSystem::Alias> HostFileSystem::Alias::Construct(const uapi::char_t* name, 
+	const FileSystem::AliasPtr& parent, const FileSystem::NodePtr& node)
+{
+	_ASSERTE(name);
+	_ASSERTE(node);
+
+	// Every Alias must have a name and a reference to a node object
+	if((!name) || (*name == 0) || (!node)) throw LinuxException(LINUX_EINVAL);
+
+	// Construct a new shared Alias instance and return it to the caller
+	return std::make_shared<Alias>(name, parent, node);
+}
+
+//-----------------------------------------------------------------------------
+// HostFileSystem::Alias::getParent
+//
+// Accesses the parent alias for this alias instance
+
+FileSystem::AliasPtr HostFileSystem::Alias::getParent(void)
+{
+	// The parent is stored as a weak reference that must be converted
+	FileSystem::AliasPtr parent = m_parent.lock();
+	if(!parent) throw LinuxException(LINUX_ENOENT);
+
+	return parent;
+}
 
 //-----------------------------------------------------------------------------
 // HOSTFILESYSTEM::DIRECTORYNODE IMPLEMENTATION
@@ -56,6 +157,37 @@
 //-----------------------------------------------------------------------------
 // HOSTFILESYSTEM::MOUNTPOINT IMPLEMENTATION
 //-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// HostFileSystem::MountPoint Constructor
+//
+// Arguments:
+//
+//	handle		- Handle to the host directory object
+//	flags		- Standard mounting flags
+//	data		- Optional custom mounting flags and data
+
+HostFileSystem::MountPoint::MountPoint(HANDLE handle, uint32_t flags, const void* data) : m_handle(handle), m_options(flags, data)
+{
+	// Determine the amount of space that needs to be allocated for the canonicalized path name string; when 
+	// providing NULL for the output, this will include the count for the NULL terminator
+	uint32_t pathlen = GetFinalPathNameByHandle(handle, nullptr, 0, FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
+	if(pathlen == 0) throw MapHostException();
+
+	// Retrieve the canonicalized path to the directory object based on the handle
+	m_hostpath.resize(pathlen);
+	pathlen = GetFinalPathNameByHandle(handle, m_hostpath.data(), pathlen, FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
+	if(pathlen == 0) throw MapHostException();
+}
+
+//-----------------------------------------------------------------------------
+// HostFileSystem::MountPoint Destructor
+
+HostFileSystem::MountPoint::~MountPoint()
+{
+	// Close the operating system handle that references the mount point
+	if(m_handle != INVALID_HANDLE_VALUE) CloseHandle(m_handle);
+}
 
 //-----------------------------------------------------------------------------
 // HOSTFILESYSTEM::PATHHANDLE IMPLEMENTATION
--- service/HostFileSystem.h
+++ service/HostFileSystem.h
@@ -25,7 +25,13 @@
 #pragma once
 
 #include <memory>
+#include <vector>
+#include <linux/errno.h>
+#include "Exception.h"
 #include "FileSystem.h"
+#include "LinuxException.h"
+#include "MountOptions.h"
+#include "Win32Exception.h"
 
 #pragma warning(push, 4)
 
@@ -60,31 +66,75 @@ public:
 
 	virtual ~HostFileSystem()=default;
 
+	// Mount
+	//
+	// Mounts the file system
+	static FileSystemPtr Mount(const uapi::char_t* source, uint32_t flags, void* data);
+
 	// FileSystem Implementation
 	//
-	virtual FileSystem::AliasPtr getRoot(void) { return nullptr; }
+	virtual FileSystem::AliasPtr getRoot(void) { return m_root; }
 
 private:
 
 	HostFileSystem(const HostFileSystem&)=delete;
 	HostFileSystem& operator=(const HostFileSystem&)=delete;
 
+	// Forward Declarations
+	//
+	class Alias;
+	class DirectoryNode;
+	class FileNode;
+	class MountPoint;
+
+	// Instance Constructor
+	//
+	HostFileSystem(const std::shared_ptr<MountPoint>& mountpoint, const std::shared_ptr<Alias>& root) : m_mountpoint(mountpoint), m_root(root) {}
+	friend class std::_Ref_count_obj<HostFileSystem>;
+
 	// Member Variables
 	//
+	std::shared_ptr<MountPoint>		m_mountpoint;	// Mount metadata/state
+	std::shared_ptr<Alias>			m_root;			// Root alias instance
 
 	//-------------------------------------------------------------------------
 	// HostFileSystem::Alias
 	//
-	class Alias
+	class Alias : public FileSystem::Alias
 	{
 	public:
 
 		virtual ~Alias()=default;
 
+		// Construct (static)
+		//
+		// Constructs a new Alias instance
+		static std::shared_ptr<Alias> Construct(const uapi::char_t* name, const FileSystem::NodePtr& node) { return Construct(name, nullptr, node); }
+		static std::shared_ptr<Alias> Construct(const uapi::char_t* name, const FileSystem::AliasPtr& parent, const FileSystem::NodePtr& node);
+
+		// FileSystem::Alias Implementation
+		//
+		virtual void					Mount(const FileSystem::NodePtr&) { throw LinuxException(LINUX_EPERM, Exception(E_NOTIMPL)); }
+		virtual void					Unmount(void) { throw LinuxException(LINUX_EPERM, Exception(E_NOTIMPL)); }
+		virtual const uapi::char_t*		getName(void) { return m_name.c_str(); }
+		virtual FileSystem::NodePtr		getNode(void) { return m_node; }
+		virtual FileSystem::AliasPtr	getParent(void);
+
 	private:
 
 		Alias(const Alias&)=delete;
 		Alias& operator=(const Alias&)=delete;
+
+		// Instance Constructor
+		//
+		Alias(const uapi::char_t* name, const FileSystem::AliasPtr& parent, const FileSystem::NodePtr& node) : m_name(name), m_parent(parent), m_node(node) {}
+		friend class std::_Ref_count_obj<Alias>;
+
+		// Member Variables
+		//
+		std::string							m_name;		// Alias name
+		FileSystem::NodePtr					m_node;		// Referenced node
+		std::weak_ptr<FileSystem::Alias>	m_parent;	// Parent alias
 	};
 
 	//-------------------------------------------------------------------------
@@ -166,12 +216,31 @@ private:
 	{
 	public:
 
-		virtual ~MountPoint()=default;
+		MountPoint(HANDLE handle, uint32_t flags, const void* data);
+		~MountPoint();
+
+		// HostPath
+		//
+		// Gets the host file system path that was mounted
+		__declspec(property(get=getHostPath)) const tchar_t* HostPath;
+		const tchar_t* getHostPath(void) const { return m_hostpath.data(); }
+
+		// Options
+		//
+		// Gets a reference to the contained MountOptions instance
+		__declspec(property(get=getOptions)) MountOptions& Options;
+		MountOptions& getOptions(void) { return m_options; }
 
 	private:
 
 		MountPoint(const MountPoint&)=delete;
 		MountPoint& operator=(const MountPoint&)=delete;
+
+		// Member Variables
+		//
+		HANDLE					m_handle;			// Mounted directory handle
+		MountOptions			m_options;			// Standard mounting options
+		std::vector<tchar_t> 	m_hostpath;			// The mounted host path
 	};
 
 	//-------------------------------------------------------------------------
