a495ae0853b48a790f197b18af1346758392ae9b
zukisoft <djp952@gmail.com>
2014-11-20 22:58:01 -0500

    Implement sys_mkdir/sys_mkdirat

---
+++ service/sys_mkdir.cpp
@@ -0,0 +1,67 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2014 Michael G. Brehm
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "SystemCall.h"
+
+#pragma warning(push, 4)
+
+// sys_mkdirat.cpp
+//
+__int3264 sys_mkdirat(const SystemCall::Context* context, int fd, const uapi::char_t* pathname, uapi::mode_t mode);
+
+//-----------------------------------------------------------------------------
+// sys_mkdir
+//
+// Creates a directory
+//
+// Arguments:
+//
+//	context		- SystemCall context object
+//	pathname	- Path to the new directory to be created
+//	mode		- Mode flags to assign to the new directory object
+
+__int3264 sys_mkdir(const SystemCall::Context* context, const uapi::char_t* pathname, uapi::mode_t mode)
+{
+	// sys_mkdir() is equivalent to sys_mkdirat(AT_FDCWD)
+	return sys_mkdirat(context, LINUX_AT_FDCWD, pathname, mode);
+}
+
+// sys32_mkdir
+//
+sys32_long_t sys32_mkdir(sys32_context_t context, const sys32_char_t* pathname, sys32_mode_t mode)
+{
+	return static_cast<sys32_long_t>(sys_mkdir(reinterpret_cast<SystemCall::Context*>(context), pathname, mode));
+}
+
+#ifdef _M_X64
+// sys64_mkdir
+//
+sys64_long_t sys64_mkdir(sys64_context_t context, const sys64_char_t* pathname, sys64_mode_t mode)
+{
+	return sys_mkdir(reinterpret_cast<SystemCall::Context*>(context), pathname, mode);
+}
+#endif
+
+//---------------------------------------------------------------------------
+
+#pragma warning(pop)
---
+++ service/sys_mkdirat.cpp
@@ -0,0 +1,83 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2014 Michael G. Brehm
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "PathSplitter.h"
+#include "SystemCall.h"
+
+#pragma warning(push, 4)
+
+//-----------------------------------------------------------------------------
+// sys_mkdirat
+//
+// Creates a directory relative to an existing directory file descriptor
+//
+// Arguments:
+//
+//	context		- SystemCall context object
+//	fd			- Previously opened directory object file descriptor
+//	pathname	- Relative path for the directory to create
+//	mode		- Mode flags to assign when creating the directory
+
+__int3264 sys_mkdirat(const SystemCall::Context* context, int fd, const uapi::char_t* pathname, uapi::mode_t mode)
+{
+	_ASSERTE(context);
+
+	try {
+
+		SystemCall::Impersonation impersonation;
+
+		// Determine if an absolute or relative pathname has been provided
+		bool absolute = ((pathname) && (pathname[0] == '/'));
+
+		// Determine the base alias from which to resolve the path
+		FileSystem::AliasPtr base = absolute ? context->Process->RootDirectory : 
+			((fd == LINUX_AT_FDCWD) ? context->Process->WorkingDirectory : context->Process->GetHandle(fd)->Alias);
+
+		// Attempt to create the directory object relative from the base alias
+		context->VirtualMachine->CreateDirectory(context->Process->RootDirectory, base, pathname, mode);
+	}
+
+	catch(...) { return SystemCall::TranslateException(std::current_exception()); }
+
+	return 0;
+}
+
+// sys32_mkdirat
+//
+sys32_long_t sys32_mkdirat(sys32_context_t context, sys32_int_t fd, const sys32_char_t* pathname, sys32_mode_t mode)
+{
+	return static_cast<sys32_long_t>(sys_mkdirat(reinterpret_cast<SystemCall::Context*>(context), fd, pathname, mode));
+}
+
+#ifdef _M_X64
+// sys64_mkdirat
+//
+sys64_long_t sys64_mkdirat(sys64_context_t context, sys64_int_t fd, const sys64_char_t* pathname, sys64_mode_t mode)
+{
+	return sys_mkdirat(reinterpret_cast<SystemCall::Context*>(context), fd, pathname, mode);
+}
+#endif
+
+//---------------------------------------------------------------------------
+
+#pragma warning(pop)
