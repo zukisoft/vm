17b6b110514942c85ad88ed1b36ce83f0747c6af
zukisoft <djp952@gmail.com>
2014-12-09 08:23:16 -0500

    MemorySection should have a virtual destructor

--- common/MemorySection.h
+++ common/MemorySection.h
@@ -43,7 +43,7 @@ public:
 
 	// Destructor
 	//
-	~MemorySection();
+	virtual ~MemorySection();
 
 	//-------------------------------------------------------------------------
 	// Type Declarations
--- service/ElfImage.h
+++ service/ElfImage.h
@@ -47,7 +47,7 @@ public:
 
 	// Destructor
 	//
-	~ElfImage()=default;
+	virtual ~ElfImage()=default;
 
 	//-------------------------------------------------------------------------
 	// Member Functions
