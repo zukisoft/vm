4747a687a7c690028d530498b16d9324c27a5b78
zukisoft <djp952@gmail.com>
2014-12-26 19:19:17 -0500

    New Bitmap class to better handle index assignments
    Ultimately can also be used for things like the TempFileSystem once it's
    done

---
+++ common/Bitmap.cpp
@@ -0,0 +1,286 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2014 Michael G. Brehm
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "Bitmap.h"
+
+#pragma warning(push, 4)
+
+// Bitmap::RtlAreBitsClear
+//
+//Bitmap::RtlAreBitsClearFunc Bitmap::RtlAreBitsClear = 
+//reinterpret_cast<BOOLEAN(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlAreBitsClear"); 
+//}());
+
+// Bitmap::RtlAreBitsSet
+//
+//Bitmap::RtlAreBitsSetFunc Bitmap::RtlAreBitsSet = 
+//reinterpret_cast<BOOLEAN(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlAreBitsSet"); 
+//}());
+
+// Bitmap::RtlClearAllBits
+//
+Bitmap::RtlClearAllBitsFunc Bitmap::RtlClearAllBits = 
+reinterpret_cast<VOID(NTAPI*)(PRTL_BITMAP)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlClearAllBits"); 
+}());
+
+// Bitmap::RtlClearBit
+//
+Bitmap::RtlClearBitFunc Bitmap::RtlClearBit = 
+reinterpret_cast<VOID(NTAPI*)(PRTL_BITMAP, ULONG)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlClearBit"); 
+}());
+
+// Bitmap::RtlClearBits
+//
+Bitmap::RtlClearBitsFunc Bitmap::RtlClearBits = 
+reinterpret_cast<VOID(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlClearBits"); 
+}());
+
+// Bitmap::RtlFindClearBits
+//
+Bitmap::RtlFindClearBitsFunc Bitmap::RtlFindClearBits = 
+reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindClearBits"); 
+}());
+
+// Bitmap::RtlFindClearBitsAndSet
+//
+Bitmap::RtlFindClearBitsAndSetFunc Bitmap::RtlFindClearBitsAndSet = 
+reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindClearBitsAndSet"); 
+}());
+
+// Bitmap::RtlFindClearRuns
+//
+//Bitmap::RtlFindClearRunsFunc Bitmap::RtlFindClearRuns = 
+//reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, PRTL_BITMAP_RUN, ULONG, BOOLEAN)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindClearRuns"); 
+//}());
+
+// Bitmap::RtlFindFirstRunClear
+//
+//Bitmap::RtlFindFirstRunClearFunc Bitmap::RtlFindFirstRunClear = 
+//reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, PULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindFirstRunClear"); 
+//}());
+
+// Bitmap::RtlFindLastBackwardRunClear
+//
+//Bitmap::RtlFindLastBackwardRunClearFunc Bitmap::RtlFindLastBackwardRunClear = 
+//reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, ULONG, PULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindLastBackwardRunClear"); 
+//}());
+
+// Bitmap::RtlFindLongestRunClear
+//
+//Bitmap::RtlFindLongestRunClearFunc Bitmap::RtlFindLongestRunClear = 
+//reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, PULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindLongestRunClear"); 
+//}());
+
+// Bitmap::RtlFindNextForwardRunClear
+//
+//Bitmap::RtlFindNextForwardRunClearFunc Bitmap::RtlFindNextForwardRunClear = 
+//reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, ULONG, PULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindNextForwardRunClear"); 
+//}());
+
+// Bitmap::RtlFindSetBits
+//
+Bitmap::RtlFindSetBitsFunc Bitmap::RtlFindSetBits = 
+reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindSetBits"); 
+}());
+
+// Bitmap::RtlFindSetBitsAndClear
+//
+Bitmap::RtlFindSetBitsAndClearFunc Bitmap::RtlFindSetBitsAndClear = 
+reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlFindSetBitsAndClear"); 
+}());
+
+// Bitmap::RtlInitializeBitMap
+//
+//Bitmap::RtlInitializeBitMapFunc Bitmap::RtlInitializeBitMap = 
+//reinterpret_cast<VOID(NTAPI*)(PRTL_BITMAP, PULONG, ULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlInitializeBitMap"); 
+//}());
+
+// Bitmap::RtlNumberOfClearBits
+//
+Bitmap::RtlNumberOfClearBitsFunc Bitmap::RtlNumberOfClearBits = 
+reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlNumberOfClearBits"); 
+}());
+
+//// Bitmap::RtlNumberOfClearBitsInRange
+////
+//Bitmap::RtlNumberOfClearBitsInRangeFunc Bitmap::RtlNumberOfClearBitsInRange = 
+//reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlNumberOfClearBitsInRange"); 
+//}());
+
+// Bitmap::RtlNumberOfSetBits
+//
+Bitmap::RtlNumberOfSetBitsFunc Bitmap::RtlNumberOfSetBits = 
+reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlNumberOfSetBits"); 
+}());
+
+//// Bitmap::RtlNumberOfSetBitsInRange
+////
+//Bitmap::RtlNumberOfSetBitsInRangeFunc Bitmap::RtlNumberOfSetBitsInRange = 
+//reinterpret_cast<ULONG(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlNumberOfSetBitsInRange"); 
+//}());
+
+// Bitmap::RtlSetAllBits
+//
+Bitmap::RtlSetAllBitsFunc Bitmap::RtlSetAllBits = 
+reinterpret_cast<VOID(NTAPI*)(PRTL_BITMAP)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlSetAllBits"); 
+}());
+
+// Bitmap::RtlSetBit
+//
+Bitmap::RtlSetBitFunc Bitmap::RtlSetBit = 
+reinterpret_cast<VOID(NTAPI*)(PRTL_BITMAP, ULONG)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlSetBit"); 
+}());
+
+// Bitmap::RtlSetBits
+//
+Bitmap::RtlSetBitsFunc Bitmap::RtlSetBits = 
+reinterpret_cast<VOID(NTAPI*)(PRTL_BITMAP, ULONG, ULONG)>([]() -> FARPROC { 
+	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlSetBits"); 
+}());
+
+// Bitmap::RtlTestBit
+//
+//Bitmap::RtlTestBitFunc Bitmap::RtlTestBit = 
+//reinterpret_cast<BOOLEAN(NTAPI*)(PRTL_BITMAP, ULONG)>([]() -> FARPROC { 
+//	return GetProcAddress(LoadLibrary(_T("ntdll.dll")), "RtlTestBit"); 
+//}());
+
+//-----------------------------------------------------------------------------
+// Bitmap Constructor
+//
+// Arguments:
+//
+//	bits		- Number of bits to define for a new, zero-initialized bitmap
+
+Bitmap::Bitmap(size_t bits)
+{
+	// The RTL_BITMAP structure is supposed to be opaque, but since it needed to
+	// be declared locally there is no reason to bother with creating duplicate
+	// member variables and passing them into RtlInitializeBitMap()
+
+	// Determine the number of 32-bit ULONG elements required for the bitmap
+	size_t ulongs = (align::up(bits, 32) >> 5);
+
+	// Allocate the storage for the bitmap and zero-initialize it
+	m_bitmap.Buffer = new ULONG[ulongs];
+	if(m_bitmap.Buffer == nullptr) throw Exception(E_OUTOFMEMORY);
+
+	// Initialize the bitmap bits to clear and set the allowable size
+	memset(m_bitmap.Buffer, 0, sizeof(ULONG) * ulongs);
+	m_bitmap.SizeOfBitMap = bits;
+}
+
+//-----------------------------------------------------------------------------
+// Bitmap Destructor
+
+Bitmap::~Bitmap()
+{
+	if(m_bitmap.Buffer) delete[] m_bitmap.Buffer;
+}
+
+//-----------------------------------------------------------------------------
+// Bitmap::getBit
+
+//bool Bitmap::getBit(size_t index)
+//{
+//	// Ensure that the index does not overrun the contained bitmap
+//	if(index > m_bitmap.SizeOfBitMap) return false;
+//
+//	return (RtlTestBit(&m_bitmap, static_cast<ULONG>(index)) == TRUE);
+//}
+
+//-----------------------------------------------------------------------------
+// Bitmap::putBit
+
+//void Bitmap::putBit(size_t index, bool value)
+//{
+//	// Ensure that the index does not overrun the contained bitmap
+//	if(index > m_bitmap.SizeOfBitMap) return;
+//
+//	// Either set or clear the bit at the specified location
+//	if(value) RtlSetBit(&m_bitmap, static_cast<ULONG>(index));
+//	else RtlClearBit(&m_bitmap, static_cast<ULONG>(index));
+//}
+
+//-----------------------------------------------------------------------------
+// Bitmap::Clear
+//
+// Clears a range of bits in the bitmap
+//
+// Arguments:
+//
+//	startbit		- Starting bit index
+//	count			- Number of bits to clear
+
+void Bitmap::Clear(size_t startbit, size_t count)
+{
+	// Verify the starting bit is in range and don't allow count to overrun
+	if(startbit > m_bitmap.SizeOfBitMap) return;
+	count = min(count, (m_bitmap.SizeOfBitMap - startbit));
+
+	RtlClearBits(&m_bitmap, static_cast<ULONG>(startbit), static_cast<ULONG>(count));
+}
+
+//-----------------------------------------------------------------------------
+// Bitmap::Set
+//
+// Sets a range of bits in the bitmap
+//
+// Arguments:
+//
+//	startbit		- Starting bit index
+//	count			- Number of bits to set
+
+void Bitmap::Set(size_t startbit, size_t count)
+{
+	// Verify the starting bit is in range and don't allow count to overrun
+	if(startbit > m_bitmap.SizeOfBitMap) return;
+	count = min(count, (m_bitmap.SizeOfBitMap - startbit));
+
+	RtlSetBits(&m_bitmap, static_cast<ULONG>(startbit), static_cast<ULONG>(count));
+}
+
+//-----------------------------------------------------------------------------
+
+#pragma warning(pop)
---
+++ common/Bitmap.h
@@ -0,0 +1,208 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2014 Michael G. Brehm
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------------------
+
+#ifndef __BITMAP_H_
+#define __BITMAP_H_
+#pragma once
+
+#include "Exception.h"
+
+#pragma warning(push, 4)
+
+//-----------------------------------------------------------------------------
+// Bitmap
+//
+// Wrapper around the Windows RTL bitmap management functions.  Can be made
+// more efficient as necessary by operating directly against the bitmap buffer
+// rather than invoking the Windows API for everything.
+
+class Bitmap
+{
+public:
+
+	// Instance Constructor / Destructor
+	//
+	Bitmap(size_t bits);
+	~Bitmap();
+	
+	//-------------------------------------------------------------------------
+	// Member Functions
+
+	// Clear
+	//
+	// Clears a specific bit, all bits, or a range of bits in the bitmap
+	void Clear(void) { RtlClearAllBits(&m_bitmap); }
+	void Clear(size_t bit) { if(bit <= m_bitmap.SizeOfBitMap) RtlClearBit(&m_bitmap, static_cast<ULONG>(bit)); }
+	void Clear(size_t startbit, size_t count);
+
+	// FindClear
+	//
+	// Finds a range of clear bits within the bitmap
+	size_t FindClear(void) { return FindClear(1); }
+	size_t FindClear(size_t quantity) { return static_cast<size_t>(RtlFindClearBits(&m_bitmap, static_cast<ULONG>(quantity), 0)); }
+
+	// FindClearAndSet
+	//
+	// Finds a range of clear bits and sets them before returning
+	size_t FindClearAndSet(void) { return FindClearAndSet(1); }
+	size_t FindClearAndSet(size_t quantity) { return static_cast<size_t>(RtlFindClearBitsAndSet(&m_bitmap, static_cast<ULONG>(quantity), 0)); }
+
+	// FindSet
+	//
+	// Finds a range of set bits within the bitmap
+	size_t FindSet(void) { return FindSet(1); }
+	size_t FindSet(size_t quantity) { return static_cast<size_t>(RtlFindSetBits(&m_bitmap, static_cast<ULONG>(quantity), 0)); }
+
+	// FindSetAndClear
+	//
+	// Finds a range of set bits and clears them before returning
+	size_t FindSetAndClear(void) { return FindSetAndClear(1); }
+	size_t FindSetAndClear(size_t quantity) { return static_cast<size_t>(RtlFindSetBitsAndClear(&m_bitmap, static_cast<ULONG>(quantity), 0)); }
+
+	// Set
+	//
+	// Sets a specific bit, all bits, or a range of bits in the bitmap
+	void Set(void) { RtlSetAllBits(&m_bitmap); }
+	void Set(size_t bit) { if(bit <= m_bitmap.SizeOfBitMap) RtlSetBit(&m_bitmap, static_cast<ULONG>(bit)); }
+	void Set(size_t startbit, size_t count);
+
+	//-------------------------------------------------------------------------
+	// Fields
+
+	// NotFound
+	//
+	// Return value from Find functions when specified range could not be found
+	static const size_t NotFound = static_cast<size_t>(-1);
+
+	//-------------------------------------------------------------------------
+	// Properties
+
+	//// Bit
+	////
+	//// Gets/sets the bit value at the specified location in the bitmap
+	//__declspec(property(get=getBit, put=putBit)) bool Bit[];
+	//bool getBit(size_t bit);
+	//void putBit(size_t bit, bool value);
+
+	// Empty
+	//
+	// Determines if the bitmap is empty
+	__declspec(property(get=getEmpty)) bool Empty;
+	bool getEmpty(void) { return RtlNumberOfSetBits(&m_bitmap) == 0; }
+
+	// Full
+	//
+	// Determines if the bitmap is full
+	__declspec(property(get=getFull)) bool Full;
+	bool getFull(void) { return RtlNumberOfClearBits(&m_bitmap) == 0; }
+
+private:
+
+	Bitmap(const Bitmap&)=delete;
+	Bitmap& operator=(const Bitmap&)=delete;
+	
+	//-------------------------------------------------------------------------
+	// Private Type Declarations
+
+	// RTL_BITMAP
+	//
+	// NTAPI structure not defined in the standard Win32 user-mode headers
+	typedef struct _RTL_BITMAP {
+
+		ULONG	SizeOfBitMap;			// Number of bits in bitmap
+		PULONG	Buffer;					// Pointer to the bitmap itself
+	
+	} RTL_BITMAP, *PRTL_BITMAP;
+
+	// RTL_BITMAP_RUN
+	//
+	// NTAPI structure not defined in the standard Win32 user-mode headers
+	//typedef struct _RTL_BITMAP_RUN {
+
+	//	ULONG	StartingIndex;
+	//	ULONG	NumberOfBits;
+
+	//} RTL_BITMAP_RUN, *PRTL_BITMAP_RUN;
+
+	// NTAPI Functions
+	//
+	//using RtlAreBitsClearFunc				= std::function<BOOLEAN(PRTL_BITMAP, ULONG, ULONG)>;
+	//using RtlAreBitsSetFunc					= std::function<BOOLEAN(PRTL_BITMAP, ULONG, ULONG)>;
+	using RtlClearAllBitsFunc				= std::function<VOID(PRTL_BITMAP)>;
+	using RtlClearBitFunc					= std::function<VOID(PRTL_BITMAP, ULONG)>;
+	using RtlClearBitsFunc					= std::function<VOID(PRTL_BITMAP, ULONG, ULONG)>;
+	using RtlFindClearBitsFunc				= std::function<ULONG(PRTL_BITMAP, ULONG, ULONG)>;
+	using RtlFindClearBitsAndSetFunc		= std::function<ULONG(PRTL_BITMAP, ULONG, ULONG)>;
+	//using RtlFindClearRunsFunc				= std::function<ULONG(PRTL_BITMAP, PRTL_BITMAP_RUN, ULONG, BOOLEAN)>;
+	//using RtlFindFirstRunClearFunc			= std::function<ULONG(PRTL_BITMAP, PULONG)>;
+	//using RtlFindLastBackwardRunClearFunc	= std::function<ULONG(PRTL_BITMAP, ULONG, PULONG)>;
+	//using RtlFindLongestRunClearFunc		= std::function<ULONG(PRTL_BITMAP, PULONG)>;
+	//using RtlFindNextForwardRunClearFunc	= std::function<ULONG(PRTL_BITMAP, ULONG, PULONG)>;
+	using RtlFindSetBitsFunc				= std::function<ULONG(PRTL_BITMAP, ULONG, ULONG)>;
+	using RtlFindSetBitsAndClearFunc		= std::function<ULONG(PRTL_BITMAP, ULONG, ULONG)>;
+	//using RtlInitializeBitMapFunc			= std::function<VOID(PRTL_BITMAP, PULONG, ULONG)>;
+	using RtlNumberOfClearBitsFunc			= std::function<ULONG(PRTL_BITMAP)>;
+	//using RtlNumberOfClearBitsInRangeFunc	= std::function<ULONG(PRTL_BITMAP, ULONG, ULONG)>;
+	using RtlNumberOfSetBitsFunc			= std::function<ULONG(PRTL_BITMAP)>;
+	//using RtlNumberOfSetBitsInRangeFunc		= std::function<ULONG(PRTL_BITMAP, ULONG, ULONG)>;
+	using RtlSetAllBitsFunc					= std::function<VOID(PRTL_BITMAP)>;
+	using RtlSetBitFunc						= std::function<VOID(PRTL_BITMAP, ULONG)>;
+	using RtlSetBitsFunc					= std::function<VOID(PRTL_BITMAP, ULONG, ULONG)>;
+	//using RtlTestBitFunc					= std::function<BOOLEAN(PRTL_BITMAP, ULONG)>;
+
+	//-------------------------------------------------------------------------
+	// Member Variables
+
+	RTL_BITMAP			m_bitmap;			// Contained RTL bitmap struct
+
+	// NTAPI
+	//
+	//static RtlAreBitsClearFunc				RtlAreBitsClear;
+	//static RtlAreBitsSetFunc				RtlAreBitsSet;
+	static RtlClearAllBitsFunc				RtlClearAllBits;
+	static RtlClearBitFunc					RtlClearBit;
+	static RtlClearBitsFunc					RtlClearBits;
+	static RtlFindClearBitsFunc				RtlFindClearBits;
+	static RtlFindClearBitsAndSetFunc		RtlFindClearBitsAndSet;
+	//static RtlFindClearRunsFunc				RtlFindClearRuns;
+	//static RtlFindFirstRunClearFunc			RtlFindFirstRunClear;
+	//static RtlFindLastBackwardRunClearFunc	RtlFindLastBackwardRunClear;
+	//static RtlFindLongestRunClearFunc		RtlFindLongestRunClear;
+	//static RtlFindNextForwardRunClearFunc	RtlFindNextForwardRunClear;
+	static RtlFindSetBitsFunc				RtlFindSetBits;
+	static RtlFindSetBitsAndClearFunc		RtlFindSetBitsAndClear;
+	//static RtlInitializeBitMapFunc			RtlInitializeBitMap;
+	static RtlNumberOfClearBitsFunc			RtlNumberOfClearBits;
+	//static RtlNumberOfClearBitsInRangeFunc	RtlNumberOfClearBitsInRange;
+	static RtlNumberOfSetBitsFunc			RtlNumberOfSetBits;
+	//static RtlNumberOfSetBitsInRangeFunc	RtlNumberOfSetBitsInRange;
+	static RtlSetAllBitsFunc				RtlSetAllBits;
+	static RtlSetBitFunc					RtlSetBit;
+	static RtlSetBitsFunc					RtlSetBits;
+	//static RtlTestBitFunc					RtlTestBit;
+};
+
+//-----------------------------------------------------------------------------
+
+#pragma warning(pop)
+
+#endif	// __BITMAP_H_
