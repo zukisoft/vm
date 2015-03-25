b9f51fda86a03d8cfdd8f6b59708483fa7de7494
zukisoft <djp952@gmail.com>
2015-03-25 01:42:56 -0400

    Refactor Waitable with a more appropriate implementation

--- service/Waitable.cpp
+++ service/Waitable.cpp
@@ -32,93 +32,162 @@
 //
 //	NONE
 
-Waitable::Waitable() : m_state(State::NotSignaled)
+Waitable::Waitable()
 {
-	// Create a Win32 manual reset event to signal state changes
-	m_statechanged = CreateEvent(nullptr, FALSE, FALSE, nullptr);
-	if(!m_statechanged) throw Win32Exception();
+	// Initialize the pending signal information to all zeros
+	memset(&m_pending, 0, sizeof(uapi::siginfo));
 }
 
 //-----------------------------------------------------------------------------
-// Waitable Destructor
-
-Waitable::~Waitable()
-{
-	CloseHandle(m_statechanged);
-}
-
-//-----------------------------------------------------------------------------
-// Waitable::PeekWaitableState
-//
-// Peeks the current waitable state without resetting the signal
+// Waitable::MaskAcceptsState (private)
 //
 // Arguments:
 //
-//	NONE
+//	mask	- Wait operation flags and options mask
+//	state	- State code to check against the provided mask
 
-Waitable::State Waitable::PeekWaitableState(void)
+bool Waitable::MaskAcceptsState(int mask, State state)
 {
-	std::lock_guard<std::mutex> critsec(m_statelock);
+	switch(state) {
 
-	// Peeking the state just returns the current value
-	return m_state;
+		// WEXITED -- Accepts State::Exited, State::Killed, State::Dumped
+		//
+		case State::Exited:
+		case State::Killed:
+		case State::Dumped:
+			return ((mask & LINUX_WEXITED) == LINUX_WEXITED);
+
+		// WSTOPPED -- Accepts State::Trapped, State::Stopped
+		//
+		case State::Trapped:
+		case State::Stopped:
+			return ((mask & LINUX_WSTOPPED) == LINUX_WSTOPPED);
+
+		// WCONTINUED -- Accepts State::Continued
+		//
+		case State::Continued:
+			return ((mask & LINUX_WCONTINUED) == LINUX_WCONTINUED);
+	}
+
+	return false;
 }
 
 //-----------------------------------------------------------------------------
-// Waitable::PopWaitableState
+// Waitable::NotifyStateChange (protected)
 //
-// Pops the current waitable state and resets the signal
+// Signals that a change in waitable object state has occurred
 //
 // Arguments:
 //
-//	NONE
+//	pid			- PID/TID of the object that has changed state
+//	state		- New state to be reported for the object
+//	status		- Current object status/exit code
 
-Waitable::State Waitable::PopWaitableState(void)
+void Waitable::NotifyStateChange(uapi::pid_t pid, State state, int32_t status)
 {
-	std::lock_guard<std::mutex> critsec(m_statelock);
+	uapi::siginfo		siginfo;			// Signal information (SIGCHLD)
 
-	// Copy out the current state and reset it to NotSignaled
-	State result = m_state;
-	m_state = State::NotSignaled;
+	// Convert the input arguments into a siginfo (SIGCHLD) structure
+	siginfo.si_signo = LINUX_SIGCHLD;
+	siginfo.si_errno = 0;
+	siginfo.si_code = static_cast<int32_t>(state);
+	siginfo.linux_si_pid = pid;
+	siginfo.linux_si_uid = 0;
+	siginfo.linux_si_status = status;
 
-	// Reset the manual reset event object
-	ResetEvent(m_statechanged);
+	// Lock the waiters collection and pending siginfo member variables
+	std::lock_guard<std::mutex> critsec(m_lock);
 
-	// Return the previously set state
-	return result;
+	// Revoke any pending signal that was not processed by a waiter
+	memset(&m_pending, 0, sizeof(uapi::siginfo));
+
+	for(auto& iterator : m_waiters) {
+
+		// Take the lock for this waiter and check that it hasn't already been spent,
+		// this is necessary to prevent a race condition wherein a second call to this
+		// function before the waiter could be removed would resignal it
+		std::unique_lock<std::mutex> critsec(iterator.lock);
+		if(iterator.siginfo->linux_si_pid != 0) continue;
+
+		// If this waiter is not interested in the signal, move on to the next one
+		if(!MaskAcceptsState(iterator.options, state)) continue;
+
+		// Write the signal information out through the provided pointer and assign
+		// the context object of this waiter to the shared result object reference
+		*iterator.siginfo = siginfo;
+		iterator.result = iterator.object;
+
+		// Signal the waiter's condition variable that a result is available
+		iterator.signal.notify_one();
+
+		// If WNOWAIT has not been specified by this waiter, the signal is consumed
+		if((iterator.options & LINUX_WNOWAIT) == 0) return;
+	}
+	
+	m_pending = siginfo;		// Save the signal for another waiter (WNOWAIT)
 }
 
 //-----------------------------------------------------------------------------
-// Waitable::SetWaitableState (protected)
+// Waitable::Wait (static)
 //
-// Sets a waitable state for the object and signals the event
+// Waits for a Waitable instance to become signaled
 //
 // Arguments:
 //
-//	state		- Waitable state to set for the object
+//	objects		- Vector of Waitable instances to be waited upon
+//	options		- Wait operation flags and options
+//	siginfo		- On success, contains resultant signal information
 
-void Waitable::SetWaitableState(State state)
+std::shared_ptr<Waitable> Waitable::Wait(const std::vector<std::shared_ptr<Waitable>>& objects, int options, uapi::siginfo* siginfo)
 {
-	// NotSignaled should not actually be set as a new state, it indicates
-	// that it has never been set or has been reset from a wait
-	_ASSERTE(state != State::NotSignaled);
+	std::condition_variable			signal;				// Signaled on a successful wait
+	std::mutex						lock;				// Condition variable synchronization object
+	std::shared_ptr<Waitable>		signaled;			// Object that was signaled		
 
-	std::lock_guard<std::mutex> critsec(m_statelock);
+	// The si_pid field of the signal information is used to detect spurious condition
+	// variable wakes as well as preventing it from being signaled multiple times
+	_ASSERTE(siginfo);
+	siginfo->linux_si_pid = 0;
 
-	// Save the new state code and signal the event; this will purposely lose 
-	// any previously signaled state that was not retrieved
-	m_state = state;
-	SetEvent(m_statechanged);
-}
+	// Register this wait with all of the provided Waitable instances
+	for(auto& iterator : objects) {
 
-//-----------------------------------------------------------------------------
-// Waitable::getWaitableStateChanged
-//
-// Gets the Win32 event object handle used to signal state changes
+		std::lock_guard<std::mutex> critsec(iterator->m_lock);
 
-HANDLE Waitable::getWaitableStateChanged(void) const
-{
-	return m_statechanged;
+		// If there is already a pending state for this Waitable instance that matches
+		// the requested wait operation mask, pull it out and stop registering waits
+		if((iterator->m_pending.linux_si_pid != 0) && (MaskAcceptsState(options, static_cast<State>(iterator->m_pending.si_code)))) {
+
+			// Pull out the pending signal information and assign the result object
+			*siginfo = iterator->m_pending;
+			signaled = iterator;
+
+			// Reset the pending signal information for the Waitable instance
+			memset(&iterator->m_pending, 0, sizeof(uapi::siginfo));
+
+			signal.notify_one();				// Condition will signal immediately
+			break;								// No need to register any more waiters
+		}
+
+		// No pending signal, register a waiter for this Waitable instance
+		else iterator->m_waiters.emplace_back(signal, lock, options, iterator, siginfo, signaled);
+	}
+
+	// Take the lock and wait for the condition variable to become signaled
+	std::unique_lock<std::mutex> critsec(lock);
+	signal.wait(critsec, [&]() -> bool { return siginfo->linux_si_pid != 0; });
+
+	// Remove this wait from the provided Waitable instances. Note that there is a race 
+	// here that would allow a new state change to resignal the condition variable before this
+	// termination code can take place -- this is taken care of in the signal function by
+	// checking that the siginfo->si_pid is still set to zero before signaling
+	for(auto iterator : objects) {
+
+		std::lock_guard<std::mutex> critsec(iterator->m_lock);
+		iterator->m_waiters.remove_if([&](const waiter_t& item) -> bool { return &item.signal == &signal; });
+	}
+
+	return signaled;			// Return Waitable instance that was signaled
 }
 
 //-----------------------------------------------------------------------------
--- service/Waitable.h
+++ service/Waitable.h
@@ -24,8 +24,12 @@
 #define __WAITABLE_H_
 #pragma once
 
-#include <concurrent_queue.h>
+#include <condition_variable>
+#include <list>
 #include <mutex>
+#include <linux/siginfo.h>
+#include <linux/signal.h>
+#include <linux/wait.h>
 #include "Win32Exception.h"
 
 #pragma warning(push, 4)
@@ -33,54 +37,37 @@
 //-----------------------------------------------------------------------------
 // Waitable
 //
-// Base class for a waitable object (processes, threads)
-
+// Base class for a waitable object (process/thread) that provides the mechanism
+// that allows the object to be waited upon for a state change
+//
 class Waitable
 {
 public:
 
 	// State
 	//
-	// Defines the waitable object state
+	// Indicates the type of state change that has occurred
 	enum class State {
 
-		NotSignaled		= 0,	// Object is not signaled
-		Suspended,				// Object has been suspended
-		Resumed,				// Object has been resumed
-		Terminated,				// Object has been terminated
+		Exited			= 1,		// Child has exited normally
+		Killed			= 2,		// Child was killed by a signal
+		Dumped			= 3,		// Child was killed and dumped
+		Trapped			= 4,		// Traced child was trapped
+		Stopped			= 5,		// Child was stopped by a signal
+		Continued		= 6,		// Child was continued by SIGCONT
 	};
 
 	// Destructor
 	//
-	virtual ~Waitable();
+	virtual ~Waitable()=default;
 
 	//-------------------------------------------------------------------------
 	// Member Functions
 
-	// PeekWaitableState
+	// Wait
 	//
-	// Peeks the current state without resetting the signal
-	State PeekWaitableState(void);
-
-	// PopWaitableState
-	//
-	// Pops the current state and resets the signal
-	State PopWaitableState(void);
-
-	//-------------------------------------------------------------------------
-	// Properties
-
-	// StatusCode (abstract)
-	//
-	// Gets the status code for the waitable object
-	__declspec(property(get=getStatusCode)) int StatusCode;
-	virtual int getStatusCode(void) = 0;
-
-	// WaitableStateChanged
-	//
-	// Exposes the event handle that can be waited on for state changes
-	__declspec(property(get=getWaitableStateChanged)) HANDLE WaitableStateChanged;
-	HANDLE getWaitableStateChanged(void) const;
+	// Waits for a waitable instance to become signaled
+	static std::shared_ptr<Waitable> Wait(const std::vector<std::shared_ptr<Waitable>>& objects, int options, uapi::siginfo* siginfo);
 
 protected:
 
@@ -91,22 +78,54 @@ protected:
 	//-------------------------------------------------------------------------
 	// Protected Member Functions
 
-	// SetWaitableState
+	// NotifyStateChange
 	//
-	// Invoked to signal that a waitable state change occurred
-	void SetWaitableState(State state);
+	// Signals that a change in waitable object state has occurred
+	void NotifyStateChange(uapi::pid_t pid, State state, int32_t status); 
 
 private:
 
 	Waitable(const Waitable&)=delete;
 	Waitable& operator=(const Waitable&)=delete;
+	
+	// waiter_t
+	//
+	// Structure used to register a wait operation with a Waitable instance
+	struct waiter_t {
+
+		// Instance Constructor
+		//
+		waiter_t(std::condition_variable& _signal, std::mutex& _lock, int _options,
+			const std::shared_ptr<Waitable>& _object, uapi::siginfo* _siginfo, std::shared_ptr<Waitable>& _result) :
+			signal(_signal), lock(_lock), options(_options), object(_object), siginfo(_siginfo), result(_result) {}
+
+		// Fields
+		//
+		std::condition_variable&			signal;			// Condition variable to signal
+		std::mutex&							lock;			// Condition variable lock object
+		int const							options;		// Wait operation mask and flags
+		const std::shared_ptr<Waitable>&	object;			// Wait operation context object
+		uapi::siginfo*						siginfo;		// Resultant signal information
+		std::shared_ptr<Waitable>&			result;			// Resultant signaled object
+
+		waiter_t(const waiter_t&)=delete;
+		waiter_t& operator=(const waiter_t&)=delete;
+	};
+
+	//-------------------------------------------------------------------------
+	// Private Member Functions
+
+	// MaskAcceptsState (static)
+	//
+	// Determines if a wait options mask accepts a specific State code
+	static bool MaskAcceptsState(int mask, State state);
 
 	//-------------------------------------------------------------------------
 	// Member Variables
-
-	State				m_state;			// Current waitable state
-	std::mutex			m_statelock;		// Synchronization object
-	HANDLE				m_statechanged;		// Waitable event object
+	
+	std::list<waiter_t>			m_waiters;		// Objects waiting for a state change
+	uapi::siginfo				m_pending;		// Unprocessed state change signal
+	std::mutex					m_lock;			// Synchronization object
 };
 
 //-----------------------------------------------------------------------------
