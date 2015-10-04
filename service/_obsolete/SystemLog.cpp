cf664d1ae11f5e82d48d92b0ab99f3f90468f800
djp952 <dpj952@gmail.com>
2015-10-04 00:17:04 -0400

    Keep old SystemLog code in _obsolete for now

---
+++ service/_obsolete/SystemLog.cpp
@@ -0,0 +1,272 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015 Michael G. Brehm
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "SystemLog.h"
+
+#include "Exception.h"
+#include "SystemInformation.h"
+
+#pragma warning(push, 4)
+
+//-----------------------------------------------------------------------------
+// SystemLog Constructor
+//
+// Arguments:
+//
+//	size		- Size of the system log ring buffer in bytes
+
+SystemLog::SystemLog(size_t size)
+{
+	// Minimum log size is the page size, maximum is constant MAX_BUFFER
+	if(size < SystemInformation::PageSize) size = SystemInformation::PageSize;
+	else if(size > MAX_BUFFER) size = MAX_BUFFER;
+
+	// Attempt to allocate the log buffer from virtual memory
+	m_buffer = MemoryRegion::Reserve(size, MEM_COMMIT);
+
+	// Initialize the top, head and tail pointers to the top of the buffer
+	m_top = m_head = m_tail = uintptr_t(m_buffer->Pointer);
+
+	// Knowing the bottom of the buffer without calculating it is useful
+	m_bottom = m_top + m_buffer->Length;
+
+	// Get the timestamp frequency in counts per second and convert to double
+	LARGE_INTEGER tsfreq;
+	QueryPerformanceFrequency(&tsfreq);
+	m_tsfreq = static_cast<double>(tsfreq.QuadPart);
+}
+
+//-----------------------------------------------------------------------------
+// SystemLog::IncrementTailPointer (private)
+//
+// Increments a tail pointer to point at the next system log entry
+//
+// Arguments:
+//
+//	tailptr		- Tail pointer to be incremented
+
+bool SystemLog::IncrementTailPointer(uintptr_t& tailptr)
+{
+	// If the tail is in the same position as the head, the buffer is empty
+	if(tailptr == m_head) return false;
+
+	// Get the length of the entry currently being pointed to
+	uint16_t entrylength = reinterpret_cast<LogEntry*>(tailptr)->entrylength;
+
+	// Increment the tail to point at the next entry in the buffer  
+	tailptr += entrylength;
+
+	// If there isn't enough room left to hold another entry header, or the length 
+	// has been set to 0xFFFF, move the tail pointer back to the top of the buffer
+	if((tailptr + sizeof(LogEntry) > m_bottom) || (entrylength == UINT16_MAX)) tailptr = m_top;
+
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+// SystemLog::Peek
+//
+// Reads entries from the system log without removing them
+//
+// Arguments:
+//
+//	format		- Output format
+//	buffer		- Output buffer
+//	len			- Output buffer size, in bytes
+
+size_t SystemLog::Peek(SystemLogFormat format, void* buffer, size_t length)
+{
+	// Peek is non-destructive, clone the member tail pointer
+	uintptr_t tailptr = m_tail;
+	return Print(format, reinterpret_cast<char*>(buffer), length, tailptr);
+}
+
+//-----------------------------------------------------------------------------
+// SystemLog::Pop
+//
+// Reads entries from the system log and removes them
+//
+// Arguments:
+//
+//	format		- Output format
+//	buffer		- Output buffer
+//	length		- Output buffer size, in bytes
+
+size_t SystemLog::Pop(SystemLogFormat format, void* buffer, size_t length)
+{
+	// Pop clears the entries as they are read, use member tail pointer
+	return Print(format, reinterpret_cast<char*>(buffer), length, m_tail);
+}
+
+//-----------------------------------------------------------------------------
+// SystemLog::Print (private)
+//
+// Prints from the tail of the system log, or counts spac required
+//
+// Arguments:
+//
+//	format		- Output string format
+//	buffer		- Destination buffer, can be nullptr
+//	length		- Length of the destination buffer
+//	tailptr		- Buffer tail pointer to use for iteration
+
+size_t SystemLog::Print(SystemLogFormat format, char* buffer, size_t length, uintptr_t& tailptr)
+{
+	if(buffer == nullptr) length = MAXSIZE_T;			// Count only
+	size_t written = 0;									// Bytes written
+
+	// Iterate over the provided tail pointer and start dumping text
+	while((tailptr != m_head) && (length > 0)) {
+
+		// Write as much as possible into the output buffer; if the entry has been
+		// truncated, discard all characters after the previous entry and break;
+		size_t out = (format == SystemLogFormat::Device) ? 
+			PrintDeviceFormat(reinterpret_cast<LogEntry*>(tailptr), buffer, length) :
+			PrintStandardFormat(reinterpret_cast<LogEntry*>(tailptr), buffer, length);
+		if(out == _TRUNCATE) { if(buffer) *buffer = 0; break; }
+		
+		if(buffer) buffer += out;			// Increment the buffer pointer
+		length -= out;						// Reduce the amount of available space
+		written += out;						// Keep track of the total written
+		IncrementTailPointer(tailptr);		// Move to the next entry
+	}
+
+	// Add space for a final trailing null when just calculating length
+	return (buffer) ? written : written + 1;
+}
+
+//-----------------------------------------------------------------------------
+// SystemLog::PrintDeviceFormat (private)
+//
+// Prints the log entry in the /dev/kmsg character device format
+//
+// Arguments:
+//
+//	entry		- Current system log entry pointer
+//	buffer		- Current pointer into the output buffer or NULL
+//	length		- Space remaining in the output buffer
+
+size_t SystemLog::PrintDeviceFormat(LogEntry const* entry, char* buffer, size_t length)
+{
+	//
+	// TODO: THIS IS JUST STANDARD FORMAT -- IMPLEMENT IT
+	//
+
+	// Check that there is at least one byte of buffer left to write into
+	if(length == 0) return 0;
+
+	// Convert the timestamp into a double representing seconds from bias
+	double time = (entry->timestamp - m_tsbias) / m_tsfreq;
+
+	// Convert the priority code into a 16-bit unsigned integer, VC does not support %hhu format
+	uint16_t priority = (entry->facility << 3 | entry->level);
+
+	// Determine if the entry ends with a line feed character or not
+	bool haslinefeed = ((entry->messagelength) && (entry->message[entry->messagelength - 1] == '\n'));
+
+	// If a buffer was specified, format directly into it, otherwise just count the characters that would be required
+	return (buffer) ? _snprintf_s(buffer, length, _TRUNCATE, "<%hu>[%#12.06f] %.*s%s", priority, time, entry->messagelength, entry->message, (haslinefeed) ? "\0" : "\n") : 
+		_scprintf("<%hu>[%#12.06f] %.*s%s", priority, time, entry->messagelength, entry->message, (haslinefeed) ? "\0" : "\n");
+}
+
+//-----------------------------------------------------------------------------
+// SystemLog::PrintStandardFormat (private)
+//
+// Prints the log entry in the standard KMSG format
+//
+// Arguments:
+//
+//	entry		- Current system log entry pointer
+//	buffer		- Current pointer into the output buffer or NULL
+//	length		- Space remaining in the output buffer
+
+size_t SystemLog::PrintStandardFormat(LogEntry const* entry, char* buffer, size_t length)
+{
+	// Check that there is at least one byte of buffer left to write into
+	if(length == 0) return 0;
+
+	// Convert the timestamp into a double representing seconds from bias
+	double time = (entry->timestamp - m_tsbias) / m_tsfreq;
+
+	// Convert the priority code into a 16-bit unsigned integer, VC does not support %hhu format
+	uint16_t priority = (entry->facility << 3 | entry->level);
+
+	// Determine if the entry ends with a line feed character or not
+	bool haslinefeed = ((entry->messagelength) && (entry->message[entry->messagelength - 1] == '\n'));
+
+	// If a buffer was specified, format directly into it, otherwise just count the characters that would be required
+	return (buffer) ? _snprintf_s(buffer, length, _TRUNCATE, "<%hu>[%#12.06f] %.*s%s", priority, time, entry->messagelength, entry->message, (haslinefeed) ? "\0" : "\n") : 
+		_scprintf("<%hu>[%#12.06f] %.*s%s", priority, time, entry->messagelength, entry->message, (haslinefeed) ? "\0" : "\n");
+}
+
+//-----------------------------------------------------------------------------
+// SystemLog::Push
+//
+// Pushes a new log entry into the buffer
+//
+// Arguments:
+//
+//	level		- Log entry level code
+//	facility	- Log entry facility code
+//	message		- Message to be pushed
+
+void SystemLog::Push(SystemLogLevel level, SystemLogFacility facility, char_t const* message)
+{
+	// The maximum message length is UINT16_MAX (64KiB)
+	size_t messagelength = std::min(strlen(message) * sizeof(char_t), static_cast<size_t>(UINT16_MAX));
+
+	// Determine the length of the entire log entry, and discard it if > UINT16_MAX
+	size_t entrylength = align::up(sizeof(LogEntry) + messagelength, __alignof(void*));
+	if(entrylength == UINT16_MAX) return;
+
+	// Check if writing this entry would wrap around to the top of the buffer
+	if(m_head + entrylength > m_bottom) {
+
+		// If the tail is currently at the top of the buffer, increment it
+		if(m_tail == m_top) IncrementTailPointer(m_tail);
+
+		// Set all unused bytes at the end of the buffer to 0xFF and move head
+		memset(reinterpret_cast<void*>(m_head), 0xFF, m_bottom - m_head);
+		m_head = m_top;
+	}
+
+	// If the head pointer is behind the tail linearly, the tail may need to be
+	// incremented until it's pushed out of the way of the new entry
+	if(m_head < m_tail) 
+		while((m_head != m_tail) && (m_head + entrylength > m_tail)) IncrementTailPointer(m_tail);
+
+	// Write the entry into the buffer at the adjusted head position
+	LogEntry* entry = reinterpret_cast<LogEntry*>(m_head);
+	QueryPerformanceCounter(reinterpret_cast<PLARGE_INTEGER>(&entry->timestamp));
+	entry->entrylength = static_cast<uint16_t>(entrylength);
+	entry->messagelength = static_cast<uint16_t>(messagelength);
+	entry->facility = static_cast<uint8_t>(facility);
+	entry->level = static_cast<uint8_t>(level);
+	memcpy(entry->message, message, messagelength);
+
+	m_head += entrylength;			// Increment the head pointer
+}
+
+//---------------------------------------------------------------------------
+
+#pragma warning(pop)
---
+++ service/_obsolete/SystemLog.h
@@ -0,0 +1,204 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015 Michael G. Brehm
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------------------
+
+#ifndef __SYSTEMLOG_H_
+#define __SYSTEMLOG_H_
+#pragma once
+
+#include "MemoryRegion.h"
+
+#pragma warning(push, 4)
+#pragma warning(disable:4200)		// zero-sized array in struct/union
+
+// SystemLogFacility
+//
+// Strongly typed enumeration defining the facility of a log entry
+enum class SystemLogFacility : uint8_t
+{
+	Kernel			= 0,			// Kernel-generated log entry
+	User			= 1,			// User-generated log entry
+
+	// TODO: there are many more of these
+};
+
+// SystemLogFormat
+//
+// Strongly typed enumeration defining the print format for the system log
+enum class SystemLogFormat
+{
+	Standard		= 0,			// Standard kmsg format
+	Device			= 1,			// Linux /dev/mksg device format
+};
+
+// SystemLogLevel
+//
+// Strongly typed enumeration defining the level of a log entry
+enum class SystemLogLevel : int8_t
+{
+	Default			= LINUX_LOGLEVEL_DEFAULT,		// Default (or last) log level
+	Emergency		= LINUX_LOGLEVEL_EMERG,			// System is unusable
+	Alert			= LINUX_LOGLEVEL_ALERT,			// Action must be taken immediately
+	Critical		= LINUX_LOGLEVEL_CRIT,			// Critical conditions
+	Error			= LINUX_LOGLEVEL_ERR,			// Error conditions
+	Warning			= LINUX_LOGLEVEL_WARNING,		// Warning conditions
+	Notice			= LINUX_LOGLEVEL_NOTICE,		// Normal but significant condition
+	Informational	= LINUX_LOGLEVEL_INFO,			// Informational
+	Debug			= LINUX_LOGLEVEL_DEBUG,			// Debug-level messages
+};
+
+//-----------------------------------------------------------------------------
+// SystemLog
+//
+// Provides the system log functionality for a virtual machine, similar to the
+// linux kernel ring buffer
+//
+// TODO: NEEDS TO BE THREAD-SAFE; start with a basic slim reader/writer or something
+// and then comment that this should be upgraded to a lock-free implementation
+// in the future -- the performance of this class is important, but not if I'm
+// never going to finish this project!
+
+class SystemLog
+{
+public:
+
+	// Instance Constructor
+	//
+	SystemLog(size_t size);
+
+	// Destructor
+	//
+	~SystemLog()=default;
+
+	//-------------------------------------------------------------------------
+	// Member Functions
+
+	// Peek
+	//
+	// Reads entries from the system log, does not clear them
+	size_t Peek(void* buffer, size_t length) { return Peek(SystemLogFormat::Standard, buffer, length); }
+	size_t Peek(SystemLogFormat format, void* buffer, size_t length);
+
+	// Pop
+	//
+	// Reads entries from the system log and removes them
+	size_t Pop(void* buffer, size_t length) { return Pop(SystemLogFormat::Standard, buffer, length); }
+	size_t Pop(SystemLogFormat format, void* buffer, size_t length);
+
+	// Push
+	// 
+	// Writes an entry into the system log
+	// TODO: needs better overloads for facility, level, should accept varargs, and so on
+	void Push(char_t const* message) { return Push(SystemLogLevel::Error, SystemLogFacility::Kernel, message); }
+	void Push(SystemLogLevel level, char_t const* message) { return Push(level, SystemLogFacility::Kernel, message); }
+	void Push(SystemLogLevel level, SystemLogFacility facility, char_t const* message);
+
+	//-------------------------------------------------------------------------
+	// Properties
+
+	// Available
+	//
+	// Gets the number of bytes available to be read from the log
+	__declspec(property(get=getAvailable)) size_t Available;
+	size_t getAvailable(void) { return Peek(nullptr, 0); }
+
+	// TODO: Need more properties, like the default console log level -- see syslog(2)
+
+	// Length
+	//
+	// The adjusted length of the system log circular buffer
+	__declspec(property(get=getLength)) size_t Length;
+	size_t getLength(void) const { return m_buffer->Length; }
+
+	// TimestampBias
+	//
+	// Gets/sets the bias to use when printing timestamp values; this value
+	// should come from a call to QueryPerformanceCounter() at startup
+	__declspec(property(get=getTimestampBias, put=putTimestampBias)) int64_t TimestampBias;
+	int64_t getTimestampBias(void) const { return m_tsbias; }
+	void putTimestampBias(int64_t value) { m_tsbias = value; }
+
+private:
+
+	SystemLog(SystemLog const&)=delete;
+	SystemLog& operator=(SystemLog const&)=delete;
+
+	// MAX_BUFFER
+	//
+	// Controls the upper boundary on the system log size
+	static size_t const MAX_BUFFER	= (1 << 23);
+
+	// LogEntry
+	//
+	// Represents the data contained in a single log entry
+	struct LogEntry
+	{
+		int64_t			timestamp;			// Entry timestamp
+		uint16_t		entrylength;		// Overall entry length
+		uint16_t		messagelength;		// Length of the message text
+		uint8_t			facility : 5;		// Facility code
+		uint8_t			level : 3;			// Entry level code
+		uint8_t			reserved[3];		// Alignment padding (repurpose me)
+		char_t			message[];			// Log message text
+
+		// message[] is followed by padding to properly align the next entry
+	};
+
+	//-------------------------------------------------------------------------
+	// Private Member Functions
+
+	// IncrementTail
+	//
+	// Increments a tail pointer to reference the next log entry
+	bool IncrementTailPointer(uintptr_t& tailptr);
+
+	// Print
+	//
+	// Prints data from the log into an output buffer using a specified tail pointer
+	size_t Print(SystemLogFormat format, char* buffer, size_t length, uintptr_t& tailptr);
+
+	// PrintDeviceFormat
+	//
+	// Formats a log entry to an output buffer in DEVICE format
+	size_t PrintDeviceFormat(LogEntry const* entry, char* buffer, size_t length);
+
+	// PrintStandardFormat
+	//
+	// Formats a log entry to an output buffer in STANDARD format
+	size_t PrintStandardFormat(LogEntry const* entry, char* buffer, size_t length);
+
+	//-------------------------------------------------------------------------
+	// Member Variables
+
+	std::unique_ptr<MemoryRegion>	m_buffer;			// Underlying buffer
+	uintptr_t						m_top;				// Top of the buffer
+	uintptr_t						m_bottom;			// Bottom of the buffer
+	uintptr_t						m_head;				// Write position
+	uintptr_t						m_tail;				// Read position
+	double							m_tsfreq;			// Timestamp frequency
+	int64_t							m_tsbias = 0;		// Timestamp bias
+};
+
+//-----------------------------------------------------------------------------
+
+#pragma warning(pop)
+
+#endif	// __SYSTEMLOG_H_
